86391368e2299eeea995908a342427777e113c3a
lch-howger <360000521@qq.com>
2021-08-17 13:57:14 +0800

    update add left-nav-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { LeftNavMenuComponent } from './left-nav-menu/left-nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    LeftNavMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/left-nav-menu/left-nav-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-nav-menu',
+  templateUrl: './left-nav-menu.component.html',
+  styleUrls: ['./left-nav-menu.component.scss']
+})
+export class LeftNavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
