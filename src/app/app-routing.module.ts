f4cb9a1a6dcd62c5db7881bf8683856457ee5571
lch-howger <360000521@qq.com>
2021-08-17 14:22:11 +0800

    update add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PageShipsComponent} from "./page-ships/page-ships.component";
+import {PageHeroesComponent} from "./page-heroes/page-heroes.component";
+import {PageBattleComponent} from "./page-battle/page-battle.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ships', component: PageShipsComponent},
+  {path: 'heroes', component: PageHeroesComponent},
+  {path: 'battle', component: PageBattleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
