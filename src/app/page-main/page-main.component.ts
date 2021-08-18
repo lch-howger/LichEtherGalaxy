0dff9d62a8e6db216da6ca5c5bd8648d159534c0
lch-howger <360000521@qq.com>
2021-08-18 11:46:28 +0800

    update test

--- src/app/page-main/page-main.component.ts
+++ src/app/page-main/page-main.component.ts
@@ -9,16 +9,26 @@ import Web3 from "web3";
 })
 export class PageMainComponent implements OnInit {
 
+  private window: any
+  private web3: any
+  public addr: string = ""
+
   constructor(private walletService: WalletService) {
+    this.web3 = new Web3("https://http-testnet.hecochain.com");
+    this.window = window
+    this.window.ethereum.enable()
   }
 
   ngOnInit(): void {
+    this.window.ethereum
+      .request({method: 'eth_accounts'})
+      .then((addrs: any) => {
+        this.addr = addrs[0]
+      })
   }
 
   testMain() {
-    alert(this.walletService.str)
   }
 
-  web3 = new Web3("https://http-testnet.hecochain.com");
 
 }
