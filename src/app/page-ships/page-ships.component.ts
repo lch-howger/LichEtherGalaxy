a067cb14e9b9800cd673570a3900b61ad209fe6b
lch-howger <360000521@qq.com>
2021-08-18 16:39:14 +0800

    Update page-ships.component.ts

--- src/app/page-ships/page-ships.component.ts
+++ src/app/page-ships/page-ships.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {WalletService} from "../service/wallet.service";
 import web3 from 'web3';
+import "./page-ships"
 
 @Component({
   selector: 'app-page-ships',
   templateUrl: './page-ships.component.html',
-  styleUrls: ['./page-ships.component.scss']
+  styleUrls: ['./page-ships.component.scss'],
 })
 export class PageShipsComponent implements OnInit {
 
+  mint: any;
+
   constructor(private walletService: WalletService) {
+    this.walletService.ethereum.enable()
   }
 
   ngOnInit(): void {
@@ -17,5 +21,31 @@ export class PageShipsComponent implements OnInit {
 
   mintShip() {
 
+
+    // this.walletService.contract.methods.lightYear_mintShip().send().then((value: any) => {
+    //   console.log(value)
+    // })
+
+    console.log(this.walletService.ethereum.isConnected())
+
+    console.log(this.walletService.addr)
+    var addr: string = this.walletService.addr
+    // this.walletService.contract.methods.lightYear_mintShip().send({
+    //     "from": addr
+    //   });
+
+    //lightYear_userFleets
+    this.walletService.contract.methods.lightYear_userFleets(0).call(
+      {
+        from:addr
+      }
+    )
+      .then((value:any)=>{
+        console.log(value)
+      })
+
+    // .then(function (receipt: any) {
+    //   console.log(receipt)
+    // });
   }
 }
