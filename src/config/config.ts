2bdcc9cc735740652d7eb3671a22ef964fbcf6fc
lch-howger <light_year_dev@163.com>
2021-08-23 15:40:58 +0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ export const config = {
   addr_hero:"0xb069d93dA7323c4d06A09eBD0bCf0B52712d5941",
   addr_home:"0xFc44fD9b5d7b8cA584eEF932F888b2272Fc985D4",
   addr_battle:"0x10820E45d023E630d26a890eca85DBeAa0d7B3e8",
-  addr_registry:"0xA184BC9E32f59A84147D0d385776CEb980a9d87D",
+  addr_registry:"0xcc1577e09086d4979a18a25d67D62E74c2C32E53",
   addr_rares: "0x5D8D7b70Ad7fF32C1334D0F7753509Ca53f7909a",
 }
