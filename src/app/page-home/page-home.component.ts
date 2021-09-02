00ef518ea9a974afa9a8af2af2270143499d72cb
lch-howger <light_year_dev@163.com>
2021-09-02 13:57:56 +0800

    update home

--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {WalletService} from "../service/wallet.service";
+import {getFleetStatusString} from "../page-fleets/fleet-status";
 
 @Component({
   selector: 'app-page-home',
@@ -14,6 +15,9 @@ export class PageHomeComponent implements OnInit {
   userInfo: any
   historyLength: any
   myBytes: any
+  fleets: any
+  shipIdArray: any
+  freeShipArray: string[] = []
 
   constructor(private walletService: WalletService) {
   }
@@ -27,6 +31,32 @@ export class PageHomeComponent implements OnInit {
     this.guardFleet = await this.walletService.homeContract.methods.getGuardFleet(this.addr).call()
     this.asset = await this.walletService.homeContract.methods.ownerAssetMap(this.addr).call()
     this.userInfo = await this.walletService.homeContract.methods.ownerUserInfoMap(this.addr).call();
+
+    this.fleets = await this.walletService.homeContract.methods.getFleets(this.addr).call()
+    this.shipIdArray = await this.walletService.shipContract.methods.getOwnerTokens().call({from: this.addr});
+
+    for (let i = 0; i < this.shipIdArray.length; i++) {
+      this.freeShipArray.push(this.shipIdArray[i])
+      console.log(this.freeShipArray.length)
+    }
+
+    for (let i = 0; i < this.fleets.length; i++) {
+      let fleet = this.fleets[i];
+      for (let j = 0; j < fleet.shipIdArray.length; j++) {
+        let index = this.freeShipArray.indexOf(fleet.shipIdArray[j]);
+        this.freeShipArray.splice(index, 1)
+      }
+    }
+
   }
 
+  getStatusString(key: any) {
+    return getFleetStatusString(key)
+  }
+
+  test() {
+    console.log(this.shipIdArray.indexOf(5))
+    console.log(this.shipIdArray.indexOf(5))
+    console.log(this.shipIdArray.indexOf(5))
+  }
 }
