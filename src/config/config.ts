89e7082230ae19b7fe3a62df987a442fba7f51cc
lch-howger <light_year_dev@163.com>
2021-09-28 13:57:45 +0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,12 @@
 export const config = {
-  addr_hero: "0x141b2a01F28d2eEABE19e9092cf08A67d68Dc6E1",
-  addr_ship: "0xBFac44Ab20376a88E455ae5775b63069c1Cf9B0B",
-  addr_battle: "0xB72B076d668826C821EB8b2DFE667b447A230e04",
-  addr_home_proxy: "0x913c6d8278aD9EFA7F419303A4c109420a5e120E",
-  addr_registry: "0xaB55b41272E54577335853EDc2dF39F455705Ccc",
+  addr_hero: "0x55e8998BB5BF6032bB7Cde133FB0F8bfbb2280be",
+  addr_ship: "0xa16c450C39C33a0637d12a7aCD862D9F495c7DB3",
+  addr_battle: "0x141b2a01F28d2eEABE19e9092cf08A67d68Dc6E1",
+  addr_home_proxy: "0x0C9E89AEe967c7Dbfe87CFa29AadF9c21F018E65",
+  addr_registry: "0x218B2f7024d37C7f2fAC480Ee908075b607FAe87",
+  addr_rares: "0x63991AC42442747e442899F977be5CF3Ac2667D5",
 
-  addr_rares: "0xE3626f778BcFf2d35a2394D6DC6584Ed871DcCF1",
+  //test
   addr_test: "0xFbD5Cbb3F13B528ab94D92d4Dd2fDE9aDbB1Bd03",
 
   //ropsten cattle
