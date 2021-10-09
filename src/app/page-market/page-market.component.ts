6bf27c87689742ef75003bfbdbe1e865b2816300
lch-howger <light_year_dev@163.com>
2021-10-09 17:25:46 +0800

    update ranking list

--- src/app/page-market/page-market.component.ts
+++ src/app/page-market/page-market.component.ts
@@ -10,11 +10,12 @@ export class PageMarketComponent implements OnInit {
 
   addr: any
   balance: any
-  fleets:any
-  erc20address:any
+  fleets: any
+  erc20address: any
+  rankingList: any
 
   constructor(private walletService: WalletService) {
-    this.erc20address="https://testnet.bscscan.com/address/"+this.walletService.resourceContract._address
+    this.erc20address = "https://testnet.bscscan.com/address/" + this.walletService.resourceContract._address
 
   }
 
@@ -26,15 +27,38 @@ export class PageMarketComponent implements OnInit {
     this.addr = await this.walletService.getAddress()
     this.balance = await this.walletService.resourceContract.methods.balanceOf(this.addr).call()
     this.fleets = await this.walletService.homeContract.methods.getFleets(this.addr).call()
+
+    let userList = await this.walletService.homeContract.methods.getUserList().call();
+    console.log(userList)
+    let list = []
+    for (let i = 0; i < userList.length; i++) {
+      let balance = await this.walletService.resourceContract.methods.balanceOf(userList[i]).call()
+      let item = {
+        addr: userList[i],
+        balance: balance
+      }
+      list.push(item)
+    }
+
+    list.sort(function (a, b) {
+      if (a.balance < b.balance) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+
+    console.log(list)
+    this.rankingList = list
   }
 
-  sellResource(index:number) {
+  sellResource(index: number) {
     this.walletService.homeContract.methods.sellResource(index).send({from: this.addr}).then(() => {
       this.refresh()
     })
   }
 
-  goHome(index:number) {
+  goHome(index: number) {
     this.walletService.homeContract.methods.goHome(index).send({from: this.addr}).then(() => {
       this.refresh()
     })
