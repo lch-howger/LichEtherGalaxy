e7b7a4ead7ddac4a276fcce9ed5a48f9c3fe6a27
lch-howger <light_year_dev@163.com>
2021-10-12 15:24:10 +0800

    update page-adventure

---
+++ src/app/page-adventure/page-adventure.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageAdventureComponent } from './page-adventure.component';
+
+describe('PageAdventureComponent', () => {
+  let component: PageAdventureComponent;
+  let fixture: ComponentFixture<PageAdventureComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageAdventureComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageAdventureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
