5fd58f13c0b13f7a0e93ff5d647505007163aa1e
lch-howger <light_year_dev@163.com>
2021-10-15 15:32:19 +0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,39 @@
 export const config = {
 
   //local
-  // addr_registry: "0x9983FE3Da9a3D0fF3377e0C9F5Bc21B8aD9d5749",
-  // addr_ship: "0x95B23f6c791A660cDc793622e4fFF73B09A61Cfd",
-  // addr_hero: "0x459f722B481eC9AA4ddED3F25FEFf599E30Fdf24",
-  // addr_rares: "0x9843638C602a33d2F3001daF0819886aDd4105A5",
-  // addr_resource: "0x2e37Df166c69549984Ab2e96a9547f9f5D5De82E",
-  // //addr_home: "0x0000000000000000000000000000000000000000",
-  // addr_battle: "0x3643d066D03e6B83DC6B1B3B3EaC4bFc0912Ddd3",
-  // addr_home_proxy: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  addr_registry: "0x58968b6994b25f407D6B194D4619EBe42CE64efa",
+  addr_ship: "0x2d787d46f3f93F7391182dA1b97BaDc169FA61C6",
+  addr_hero: "0x04b27a520719579955d8A8faF1b4AD8E8E09C997",
+  addr_rares: "0xd5C2c2bfB153cA2d14cFe5Fe21256d72d2302D5C",
+  addr_resource: "0x90A4ba759b82914A190F0acd6a87CfA354BAa334",
+  //addr_home: "0xcC7510299217dDB47D2e53EaB0d50d3a07010fD3",
+  addr_battle: "0xD310c4638b57f881f66350832c320F426dCE29E3",
+  addr_home_proxy: "0x2Cd94b3512f342E9Dd2CF4b685de9F12C192eA38",
 
-  //bsc
-  addr_registry: "0xAC6aFf5DdbA13fcb188c1E20B722908C3F7D45be",
-  addr_ship: "0x788E5C24924A5Fe67e1ce0a521F59Dbf21F535c1",
-  addr_hero: "0x49515eCD099eb844966cF043120785207c58f67B",
-  addr_resource: "0x64D5FdcfbBe8029255D71a8101b25d117c33d1D9",
-  //addr_home: "0x78193bBF4A7E5E8fB73F6783157F5DBb93f7D2b2",
-  addr_battle: "0x1461Da163376745CB4a8bb00E1fC9Af778453d32",
-  addr_home_proxy: "0xc7e16420D516e444F68329175C8203760A44b5E3",
+  addr_token_iron: "0x35a4B78d444a6cD34440c19B4865CC6ac540fA49",
+  addr_token_copper: "0xCc84c87Bb1e4EAfb6b8Dbde9e3b21c9b21139c82",
+  addr_token_silver: "0xE4109069b04821f7BCAdCDd386Ef5cC2ac0b6b47",
+  addr_token_gold: "0xec3D8d6FE5D674EA7C6ac53594B2a6007cfCcbbD",
+  add_staking:"0xe18DC55CeF8730a5cE99aC7911bE50fc774af899",
+  addr_token_usd: "0x37cdBE316eeF17a6E3Cde4B80307b8f2604441C8",
+
+
+  // //bsc
+  // addr_registry: "0xAC6aFf5DdbA13fcb188c1E20B722908C3F7D45be",
+  // addr_ship: "0x788E5C24924A5Fe67e1ce0a521F59Dbf21F535c1",
+  // addr_hero: "0x49515eCD099eb844966cF043120785207c58f67B",
+  // addr_resource: "0x64D5FdcfbBe8029255D71a8101b25d117c33d1D9",
+  // //addr_home: "0x78193bBF4A7E5E8fB73F6783157F5DBb93f7D2b2",
+  // addr_battle: "0x1461Da163376745CB4a8bb00E1fC9Af778453d32",
+  // addr_home_proxy: "0xc7e16420D516e444F68329175C8203760A44b5E3",
+  //
+  // addr_token_resource: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  // addr_token_iron: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  // addr_token_copper: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  // addr_token_silver: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  // addr_token_gold: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
+  // add_staking:"",
+  // addr_token_usd: "0x8d3e7F68B6C0D5533e6F4e6d3f2020d6970FB95b",
 
   //test
   addr_test: "0xFbD5Cbb3F13B528ab94D92d4Dd2fDE9aDbB1Bd03",
