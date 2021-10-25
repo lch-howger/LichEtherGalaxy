42d818e7c39ce9ca8a40a148cf8056f1827de1b7
lch-howger <light_year_dev@163.com>
2021-10-25 16:02:48 +0800

    update tabs

--- src/app/tab-fleets/tab-fleets.component.ts
+++ src/app/tab-fleets/tab-fleets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WalletService} from "../service/wallet.service";
 
 @Component({
@@ -9,6 +9,9 @@ import {WalletService} from "../service/wallet.service";
 export class TabFleetsComponent implements OnInit {
 
   addr: any
+  fleets: any = []
+  nowTab = 0
+  loaded: any = false
 
   constructor(private walletService: WalletService) {
 
@@ -20,6 +23,8 @@ export class TabFleetsComponent implements OnInit {
 
   async refresh() {
     this.addr = await this.walletService.getAddress()
+    this.fleets = await this.walletService.homeContract.methods.getFleets(this.addr).call()
+    this.loaded = true
   }
 
 }
