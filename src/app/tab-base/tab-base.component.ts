54b1553966754397212af98c081c5405553a0a98
lch-howger <light_year_dev@163.com>
2021-10-25 13:00:27 +0800

    update ts base info

--- src/app/tab-base/tab-base.component.ts
+++ src/app/tab-base/tab-base.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WalletService} from "../service/wallet.service";
 
 @Component({
   selector: 'app-tab-base',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabBaseComponent implements OnInit {
 
-  constructor() { }
+  addr: any
+
+  constructor(private walletService: WalletService) {
+
+  }
 
   ngOnInit(): void {
+    this.refresh()
+  }
+
+  async refresh() {
+    this.addr = await this.walletService.getAddress()
   }
 
 }
